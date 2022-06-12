0e53b50d71a1a97bdc6413eecd2b416053410e55
Arturo Serna Leon <arturo@funda.nl>
2022-06-12 18:29:57 +0200

    Fix typo in comment

--- 1.03/steps.sh
+++ 1.03/steps.sh
@@ -8,7 +8,7 @@ docker push $image
 k3d cluster create -a 2
 kubectl config use-context k3d-k3s-default
 
-##main changed compared to 1.01
+# main change compared to 1.01
 kubectl apply -f manifests/deployment.yaml
 
 kubectl get deployments
--- 1.04/steps.sh
+++ 1.04/steps.sh
@@ -12,7 +12,7 @@ docker push $image
 k3d cluster create -a 2
 kubectl config use-context k3d-k3s-default
 
-##main changed compared to 1.02
+# main change compared to 1.02
 kubectl apply -f manifests/deployment.yaml
 
 kubectl get deployments
