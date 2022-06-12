056002078cca6b1e0ac14a911023c5da7528ca5a
Arturo Serna Leon <arturo@funda.nl>
2022-06-12 18:27:45 +0200

    Add exercise 104

---
+++ 1.04/readme.md
@@ -0,0 +1,8 @@
+# Exercise 1.04: Project v0.2
+
+Create a deployment.yaml for the project.
+
+You won't have access to the port yet but that'll come soon.
+
+## Solution remarks
+- The commands performed to deploy the app are available in ```steps.sh```.
---
+++ 1.04/steps.sh
@@ -0,0 +1,23 @@
+appName=p104
+dockerUsername=sernalen
+image=$dockerUsername/k8scourse-${appName}:latest
+
+set -o xtrace
+
+kubectl delete --all deployments
+
+docker build . -t $image
+docker push $image
+
+k3d cluster create -a 2
+kubectl config use-context k3d-k3s-default
+
+##main changed compared to 1.02
+kubectl apply -f manifests/deployment.yaml
+
+kubectl get deployments
+kubectl get pods
+pod=$( kubectl get pods | grep $appName | awk '{print $1;}' )
+
+sleep 5
+kubectl logs -f $pod
