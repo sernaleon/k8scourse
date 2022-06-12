8b5d611a97942df6132a6257dda95c4171f6c65a
Arturo Serna Leon <arturo@funda.nl>
2022-06-12 18:06:24 +0200

    Extract literals in variables

--- 1.01/steps.sh
+++ 1.01/steps.sh
@@ -1,9 +1,16 @@
-image=sernalen/k8scourse-p1.01-log-output:latest
+appName=p101
+dockerUsername=sernalen
+image=$dockerUsername/k8scourse-${appName}:latest
+
 docker build . -t $image
 docker push $image
-kubectl create deployment randomstring --image=$image
+
+k3d cluster create -a 2
+kubectl config use-context k3d-k3s-default
+
+kubectl create deployment $appName --image=$image
 kubectl get deployments
 kubectl get pods
-pod=$( kubectl get pods | grep randomstring| awk '{print $1;}' )
+pod=$( kubectl get pods | grep $appName | awk '{print $1;}' )
 echo $pod
 kubectl logs -f $pod
